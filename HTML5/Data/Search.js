225c4dec2325410cc7fe6510ab06e6a3c4ca31d0
ChrisChan2003 <c.chan2003@gmail.com>
2026-03-12 15:15:55 -0400

    Delete HTML5 directory

--- HTML5/Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Introduction-Topics/Accessibility-standards.htm'],s:['0'],p:[['0',5],['model',1]]});
--- HTML5/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=639088434908931564'
-});
